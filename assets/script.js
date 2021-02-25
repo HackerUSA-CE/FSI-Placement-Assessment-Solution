9aadfe3844ea9a08754f3e2a15b9fed9ca0d2a4b
brandiw <brandimichelle.0911@gmail.com>
2021-02-25 11:41:03 -0800

    Adding name credit

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,14 @@
-// Global Variables - we'll use these to track the counts of each cookie type
+// Global Variables 
+// First, tell us your name
+let yourName = "Jane Doe" // HINT: Replace this with your own name!
+
+// We'll use these variables to track the counts of each cookie type
 let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
+
+// Code to update name display
+document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Ginger Bread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
